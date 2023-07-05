62ed9d74a4c67ea18481df130cd7b8b69748e4fe
kkotburi <128236313+kkotburi@users.noreply.github.com>
2023-07-05 11:06:38 +0900

    clean up unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,7 @@
 import React from "react";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((json) => {
-        setData([...json]);
-        return console.log(json);
-      });
-  }, []);
-
-  return (
-    <>
-      <h3>JSONPlacehoder DATA</h3>
-      {data.map((item) => {
-        return (
-          <div
-            key={item.id}
-            style={{
-              border: "1px solid black",
-              margin: "5px",
-            }}
-          >
-            <ul>
-              <li>{item.userId}</li>
-              <li>{item.id}</li>
-              <li>{item.title}</li>
-              <li>{item.body}</li>
-            </ul>
-          </div>
-        );
-      })}
-    </>
-  );
+  return <div>App</div>;
 }
 
 export default App;
